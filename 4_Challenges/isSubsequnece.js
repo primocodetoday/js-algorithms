5260127cf4d47b9c6c5beff7a90b6cf88a81817e
primocodetoday <primocodetoday@gmail.com>
2020-09-16 21:09:31 +0200

    1 part of challenges finished

---
+++ 4_Challenges/isSubsequnece.js
@@ -0,0 +1,18 @@
+﻿const isSubsequence = (a, b) => {
+  let i = 0;
+  let j = 0;
+
+  if (!a) return true;
+
+  while (j < b.length) {
+    if (b[j] === a[i]) i++;
+    if (i === a.length) return true;
+    j++;
+  }
+  return false;
+};
+
+console.log(isSubsequence('hello', 'hello World')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('abc', 'acb')); // false
