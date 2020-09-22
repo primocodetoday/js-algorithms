3a67ab2c2f7035774dc22d4404fad3c7eee2bbad
primocodetoday <primocodetoday@gmail.com>
2020-09-22 14:35:37 +0200

    mergeSort algorithm

--- 10_Advance_Sort_Algorithms/mergeSort.js
+++ 10_Advance_Sort_Algorithms/mergeSort.js
@@ -1,14 +1,12 @@
-﻿// Merging Arrays Pseudocode
-// Create an empty array, take a look at the smallest values in each input array
-// While there are still values we haven't looked at...
-// If the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
-// If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
-// Once we exhaust one array, push in all remaining values from the other array
+﻿// mergeSort Pseudocode
+// Break up the array into halves until you have arrays that are empty or have one element
+// Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+// Once the array has been merged back together, return the merged (and sorted!) array
 
 const merge = (arr1, arr2) => {
   const result = [];
-  let j = 0;
   let i = 0;
+  let j = 0;
 
   while (result.length < arr1.length + arr2.length) {
     if (arr1[i] < arr2[j]) {
@@ -22,4 +20,16 @@ const merge = (arr1, arr2) => {
   return result;
 };
 
-console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+const mergeSort = (array) => {
+  if (array.length <= 1) return array;
+
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+
+  return merge(left, right);
+};
+
+console.log(mergeSort([1, 10, 50, 2, 14, 99, 100]));
+
+// O(n log n)
