78fde2c1ccd9b92318e4ae6ac2e5aa3d821813f4
primocodetoday <primocodetoday@gmail.com>
2020-10-24 23:59:21 +0200

    feat(DLL Class): added push()

--- 12_Doubly_Linked_Lists/DoublyLinkedLists.js
+++ 12_Doubly_Linked_Lists/DoublyLinkedLists.js
@@ -1,22 +1,27 @@
-﻿export class DoublyLinkedList {
+﻿import { Node } from './Node';
+
+export class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
 
-  // push(value) {
-  //   const newNode = new Node(value);
-  //   if (!this.head) {
-  //     this.head = newNode;
-  //     this.tail = newNode;
-  //   } else {
-  //     this.tail.next = newNode;
-  //     this.tail = newNode;
-  //   }
-  //   this.length++;
-  //   return this;
-  // }
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 
   // pop() {
   //   if (!this.head) return undefined;
--- 12_Doubly_Linked_Lists/DoublyLinkedLists.test.js
+++ 12_Doubly_Linked_Lists/DoublyLinkedLists.test.js
@@ -1,8 +1,6 @@
 ﻿import { Node } from './Node';
 import { DoublyLinkedList } from './DoublyLinkedLists';
 
-jest.mock('./Node.js');
-
 describe('DoublyLinkedList Class works properly', () => {
   it('and creates new List', () => {
     const list = new DoublyLinkedList();
@@ -10,5 +8,19 @@ describe('DoublyLinkedList Class works properly', () => {
     expect(list.length).toBe(0);
     expect(list.head).toBe(null);
     expect(list.tail).toBe(null);
+  });
+
+  it('and Pop method works fine', () => {
+    const list = new DoublyLinkedList();
+
+    list.push(5);
+    expect(list.length).toBe(1);
+    expect(list.head).toEqual({ value: 5, next: null, prev: null });
+    expect(list.tail).toEqual({ value: 5, next: null, prev: null });
+
+    list.push(10);
+    expect(list.length).toBe(2);
+    expect(list.head.value).toEqual(5);
+    expect(list.tail.value).toEqual(10);
   });
 });
