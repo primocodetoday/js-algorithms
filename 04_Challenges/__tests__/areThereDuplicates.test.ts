0db9758755a62ba3505e9c594026a9a94cf9b949
primocodetoday <primocodetoday@gmail.com>
2020-11-06 10:32:46 +0100

    ref: areThereDuplicates rewritten and tested in TS

---
+++ 04_Challenges/__tests__/areThereDuplicates.test.ts
@@ -0,0 +1,25 @@
+﻿import { areThereDuplicates, areThereDuplicatesES6 } from '../areThereDuplicates';
+
+test('areThereDuplicates() works as expected', () => {
+  const test1 = [1, 2, 3];
+  const test2 = [1, 2, 2];
+  const test3 = ['a', 'b', 'c', 'a'];
+  const test4 = ['f', 'b', 'c', 'a', 'a'];
+
+  expect(areThereDuplicates(...test1)).toBeFalsy();
+  expect(areThereDuplicates(...test2)).toBeTruthy();
+  expect(areThereDuplicates(...test3)).toBeTruthy();
+  expect(areThereDuplicates(...test4)).toBeTruthy();
+});
+
+test('areThereDuplicatesES6() works as expected', () => {
+  const test1 = [1, 2, 3];
+  const test2 = [1, 2, 2];
+  const test3 = ['a', 'b', 'c', 'a'];
+  const test4 = ['f', 'b', 'c', 'a', 'a'];
+
+  expect(areThereDuplicatesES6(...test1)).toBeFalsy();
+  expect(areThereDuplicatesES6(...test2)).toBeTruthy();
+  expect(areThereDuplicatesES6(...test3)).toBeTruthy();
+  expect(areThereDuplicatesES6(...test4)).toBeTruthy();
+});
--- 04_Challenges/areThereDuplicates.ts
+++ 04_Challenges/areThereDuplicates.ts
@@ -1,6 +1,6 @@
 ﻿// Implement a function called,areThereDuplicates, which accepts a variable number of arguments,and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
 
-const areThereDuplicates = (...args) => {
+export const areThereDuplicates = <T extends string | number>(...args: T[]) => {
   const arr = args;
 
   let start = 0;
@@ -16,12 +16,8 @@ const areThereDuplicates = (...args) => {
   return false;
 };
 
-console.log(areThereDuplicates(1, 2, 3));
-console.log(areThereDuplicates(1, 2, 2));
-console.log(areThereDuplicates('a', 'b', 'c', 'a'));
-console.log(areThereDuplicates('f', 'b', 'c', 'a', 'a'));
-
 // Solution with new Set
-// function areThereDuplicates() {
-//   return new Set(arguments).size !== arguments.length; genius
-// }
+export const areThereDuplicatesES6 = <T extends string | number>(...args: T[]) => {
+  return new Set(args).size !== args.length;
+  //  genius ;)
+};
