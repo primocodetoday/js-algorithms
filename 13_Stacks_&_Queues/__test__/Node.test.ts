a6b6bc7f158296744b5f865df7a9795d643fe241
primocodetoday <primocodetoday@gmail.com>
2020-11-14 19:12:05 +0100

    dev: stack structure added

---
+++ 13_Stacks_&_Queues/__test__/Node.test.ts
@@ -0,0 +1,8 @@
+﻿import { Node } from '../Node';
+
+it('Creates Node properly', () => {
+  const newNode = new Node('5');
+
+  expect(newNode.value).toBe('5');
+  expect(newNode.next).toBe(null);
+});
