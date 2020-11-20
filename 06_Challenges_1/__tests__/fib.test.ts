a584a9d7312bcf15c0fe8b4b3666503c7ac3785c
primocodetoday <primocodetoday@gmail.com>
2020-11-20 23:57:19 +0100

    ref: fib function tested

---
+++ 06_Challenges_1/__tests__/fib.test.ts
@@ -0,0 +1,10 @@
+﻿import { fib } from '../fib';
+
+// 1 + 1 + 2 + 3 + 5 + 8;
+
+test('fib() works as expected', () => {
+  expect(fib(4)).toBe(3);
+  expect(fib(10)).toBe(55);
+  expect(fib(28)).toBe(317811);
+  expect(fib(35)).toBe(9227465);
+});
---
+++ 06_Challenges_1/fib.ts
@@ -0,0 +1,4 @@
+﻿export const fib = (nr: number): number => {
+  if (nr <= 2) return 1;
+  return fib(nr - 1) + fib(nr - 2);
+};
