f23678abff6e258afb978fd47aa4cdc265b433c1
primocodetoday <primocodetoday@gmail.com>
2020-12-02 23:33:54 +0100

    dev: AdventOfCode2020 added

---
+++ 99_AdventOfCode2020/AdventOfCode_1.ts
@@ -0,0 +1,42 @@
+﻿import { arr } from './inputs/input1';
+
+const sorted = arr.sort((a, b) => (a > b ? 1 : -1));
+
+const findSumOf2 = <T extends number>(arr: T[], nr: T) => {
+  let begin = 0;
+  let end = arr.length - 1;
+  while (begin < end) {
+    const sum = arr[begin] + arr[end];
+    if (sum === nr) {
+      return [arr[begin], arr[end]];
+    }
+    if (sum > nr) {
+      end--;
+    } else {
+      begin++;
+    }
+  }
+  return undefined;
+};
+
+const numbers2 = findSumOf2(sorted, 2020);
+const result2 = numbers2 ? numbers2.reduce((prev, acc) => prev * acc, 1) : null;
+
+console.log(result2);
+
+const findSumOf3 = <T extends number>(arr: T[], nr: T) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      for (let k = 2; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] === nr) {
+          return [arr[i], arr[j], arr[k]];
+        }
+      }
+    }
+  }
+  return undefined;
+};
+
+const numbers3 = findSumOf3(sorted, 2020);
+const result3 = numbers3 ? numbers3.reduce((prev, acc) => prev * acc, 1) : null;
+console.log(result3);
---
+++ 99_AdventOfCode2020/AdventOfCode_2.ts
@@ -0,0 +1,40 @@
+﻿const readFile = require('fs').readFileSync;
+
+const res = readFile('./inputs/2.input', 'utf-8')
+  .split('\n')
+  .filter((s: any) => s.trim())
+  .filter((line: any) => {
+    const [rule, password] = line.split(': ');
+    let [, min, max, letter] = rule.match(/(\d+)\-(\d+) (\w)/);
+    min = parseInt(min, 10);
+    max = parseInt(max, 10);
+
+    const count = password.split('').filter((l: any) => l === letter).length;
+
+    return count >= min && count <= max;
+  });
+
+const res2 = readFile('./2.input', 'utf-8')
+  .split('\n')
+  .filter((s: any) => s.trim())
+  .filter((line: any) => {
+    const [rule, password] = line.split(': ');
+    let [, min, max, letter] = rule.match(/(\d+)\-(\d+) (\w)/);
+    min = parseInt(min, 10) - 1;
+    max = parseInt(max, 10) - 1;
+
+    const split = password.split('');
+
+    if (split[min] === letter) {
+      if (split[max] !== letter) {
+        return true;
+      }
+    }
+    if (split[max] === letter) {
+      if (split[min] !== letter) {
+        return true;
+      }
+    }
+  });
+
+console.log(res2.length);
