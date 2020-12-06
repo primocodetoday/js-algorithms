c70939483868f9d5522427599a48cf00981bbd5c
primocodetoday <primocodetoday@gmail.com>
2020-12-06 11:53:46 +0100

    dev: adventOfCode_6 added

---
+++ 99_Advent_Of_Code_2020/AdventOfCode_6.ts
@@ -0,0 +1,37 @@
+﻿export const countAnswers1 = (input: string) => {
+  let answers = 0;
+
+  const result = input
+    .trim()
+    .split('\n\n')
+    .map((line) => line.split(/[ \n]+/))
+    .map((line) => line.join(''))
+    .map((line) => line.split(''))
+    .map((line) => {
+      const set = new Set(line);
+      const str = [...set].join('');
+      return str;
+    });
+
+  result.map((group) => (answers += group.length));
+  return answers;
+};
+
+export const countAnswers2 = (input: string) => {
+  let answers = 0;
+
+  const groups = input.trim().split('\n\n');
+
+  function countSameAnswers(string: string) {
+    let array = string.split('\n');
+    const answer = new Set(string.replace(/[^a-z]/g, ''));
+    const inCommon = Array.from(answer).filter((q) => array.every((answer) => answer.includes(q)));
+    return inCommon.length;
+  }
+
+  for (const group of groups) {
+    answers += countSameAnswers(group);
+  }
+
+  return answers;
+};
---
+++ 99_Advent_Of_Code_2020/__tests__/AdventOfCode_6.test.ts
@@ -0,0 +1,10 @@
+﻿import { countAnswers1, countAnswers2 } from '../AdventOfCode_6';
+import { input } from '../inputs/input6';
+
+it('countAnswers1 works fine and return number', () => {
+  expect(countAnswers1(input)).toBe(6437);
+});
+
+it('countAnswers2 works fine and return number', () => {
+  expect(countAnswers2(input)).toBe(3229);
+});
