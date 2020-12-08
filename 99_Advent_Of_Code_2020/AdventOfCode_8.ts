fa758f7e167fed9e10ff92081c87edb6657e1212
primocodetoday <primocodetoday@gmail.com>
2020-12-08 11:00:05 +0100

    dev: adventOfCode_8 part1 added

---
+++ 99_Advent_Of_Code_2020/AdventOfCode_8.ts
@@ -0,0 +1,44 @@
+﻿export enum Operation {
+  nop = 'nop',
+  jmp = 'jmp',
+  acc = 'acc',
+}
+
+export const assembler = (input: string) => {
+  const code = input
+    .trim()
+    .split('\n')
+    .map((line) => {
+      const [o, a] = line.split(' ');
+      return { op: o, arg: parseInt(a, 10) };
+    });
+
+  let acc = 0;
+  const executed = new Set();
+
+  for (let i = 0; i < code.length; ) {
+    const inst = code[i];
+
+    if (executed.has(i)) {
+      break;
+    } else {
+      executed.add(i);
+    }
+
+    switch (inst.op) {
+      case Operation.jmp:
+        i += inst.arg;
+        break;
+      case Operation.acc:
+        acc += inst.arg;
+        i++;
+        break;
+      default:
+      case Operation.nop:
+        i++;
+        break;
+    }
+  }
+
+  return acc;
+};
---
+++ 99_Advent_Of_Code_2020/__tests__/AdventOfCode_8.test.ts
@@ -0,0 +1,6 @@
+﻿import { assembler } from '../AdventOfCode_8';
+import { input } from '../inputs/input8';
+
+it('assember works fine and return number', () => {
+  expect(assembler(input)).toBe(1654);
+});
